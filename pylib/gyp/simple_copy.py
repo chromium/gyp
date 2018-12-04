f2dca32f785648a03440ff030625150590a52372
Dirk Pranke <dpranke@chromium.org>
2018-12-03 20:35:18 -0800

    Update simple_copy.py for Python3 types.
    Python 3 gets rid of 'str', 'long', so the file needed to be
    tweaked slightly.
    
    Bug: gyp:36
    Change-Id: I7c820dfd6dfb5bfcf1a8ed315d67095557822757
    Reviewed-on: https://chromium-review.googlesource.com/c/1360353
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- pylib/gyp/simple_copy.py
+++ pylib/gyp/simple_copy.py
@@ -28,8 +28,19 @@ _deepcopy_dispatch = d = {}
 def _deepcopy_atomic(x):
   return x
 
-for x in (type(None), int, long, float,
-          bool, str, unicode, type):
+try:
+  _string_types = (str, unicode)
+# There's no unicode in python3
+except NameError:
+  _string_types = (str, )
+
+try:
+  _integer_types = (int, long)
+# There's no long in python3
+except NameError:
+  _integer_types = (int, )
+
+for x in (type(None), float, bool, type) + _integer_types + _string_types:
   d[x] = _deepcopy_atomic
 
 def _deepcopy_list(x):
