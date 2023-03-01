c6d8b9f7ee355cff1531b0f369cd338a50baeb07
Raymond Zhao <raymondzhao@microsoft.com>
2023-03-01 09:29:31 -0800

    Add SpectreMitigation attribute for msvs
    This CL allows gyp to recognize the SpectreMitigation msvs_attribute
    and add it to the generated vcxproj file for MSBuild.
    Possible values for the attribute are Spectre, SpectreLoad,
    SpectreLoadCF, and false.
    
    The /Qspectre compiler option is not enough to add
    full Spectre mitigation, because even though it causes MSBuild
    to add additional instructions to the generated object files,
    it does not cause MSBuild to link against Spectre-mitigated
    libraries provided by Visual Studio.
    
    Change-Id: I8294395659354f3ce60a355622da4b026abcceba
    Reviewed-on: https://chromium-review.googlesource.com/c/external/gyp/+/4265144
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- pylib/gyp/generator/msvs.py
+++ pylib/gyp/generator/msvs.py
@@ -2740,6 +2740,10 @@ def _GetMSBuildConfigurationDetails(spec, build_file):
     config_type = msbuild_attributes.get('ConfigurationType')
     _AddConditionalProperty(properties, condition, 'ConfigurationType',
                             config_type)
+    spectre_mitigation = msbuild_attributes.get('SpectreMitigation')
+    if spectre_mitigation:
+      _AddConditionalProperty(properties, condition, 'SpectreMitigation',
+                              spectre_mitigation)
     if config_type == 'Driver':
       _AddConditionalProperty(properties, condition, 'DriverType', 'WDM')
       _AddConditionalProperty(properties, condition, 'TargetVersion',
@@ -2821,6 +2825,8 @@ def _ConvertMSVSBuildAttributes(spec, config, build_file):
       msbuild_attributes[a] = _ConvertMSVSCharacterSet(msvs_attributes[a])
     elif a == 'ConfigurationType':
       msbuild_attributes[a] = _ConvertMSVSConfigurationType(msvs_attributes[a])
+    elif a == 'SpectreMitigation':
+      msbuild_attributes[a] = msvs_attributes[a]
     else:
       print('Warning: Do not know how to convert MSVS attribute ' + a)
   return msbuild_attributes
